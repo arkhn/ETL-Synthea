da52f08dda62db8c8f2d0300081e879ef5bc82bd
simonvadee <simon.vadee@gmail.com>
2021-12-10 09:49:30 +0000

    fix(create_synthea_tables): quote reserved oracle keywords

--- inst/sql/sql_server/synthea_version/v270/create_synthea_tables.sql
+++ inst/sql/sql_server/synthea_version/v270/create_synthea_tables.sql
@@ -1,49 +1,49 @@
 
 --HINT DISTRIBUTE_ON_RANDOM
 create table @synthea_schema.allergies (
-start         date,
-stop          date,
+"start"         date,
+"stop"          date,
 patient       varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(255)
+"description"   varchar(255)
 );
 
 --HINT DISTRIBUTE_ON_RANDOM
 create table @synthea_schema.careplans (
 id            varchar(1000),
-start         date,
-stop          date,
+"start"         date,
+"stop"          date,
 patient       varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(255),
+"description"   varchar(255),
 reasoncode   varchar(255),
 reasondescription   varchar(255)
 );
 
 --HINT DISTRIBUTE_ON_RANDOM
 create table @synthea_schema.conditions (
-start         date,
-stop          date,
+"start"         date,
+"stop"          date,
 patient       varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(255)
+"description"   varchar(255)
 );
 
 --HINT DISTRIBUTE_ON_RANDOM
 create table @synthea_schema.encounters (
 id            		varchar(1000),
-start         		date,
-stop							date,
+"start"         		date,
+"stop"							date,
 patient       		varchar(1000),
 organization   		varchar(1000),
-provider			varchar(1000),
+"provider"			varchar(1000),
 payer			varchar(1000),
 encounterclass		varchar(1000),
 code          		varchar(100),
-description   		varchar(255),
+"description"   		varchar(255),
 base_encounter_cost numeric,
 total_claim_cost		numeric,
 payer_coverage		numeric,
@@ -57,7 +57,7 @@ create table @synthea_schema.immunizations (
 patient       varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(255),
+"description"   varchar(255),
 base_cost	numeric
 );
 
@@ -80,13 +80,13 @@ procedure_code			varchar(255)
 
 --HINT DISTRIBUTE_ON_RANDOM
 create table @synthea_schema.medications (
-start         date,
-stop          date,
+"start"         date,
+"stop"          date,
 patient       varchar(1000),
 payer		varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(1000),
+"description"   varchar(1000),
 base_cost	  numeric,
 payer_coverage		numeric,
 dispenses			int,
@@ -101,19 +101,19 @@ create table @synthea_schema.observations (
 patient       varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(255),
-value     		varchar(1000),
+"description"   varchar(255),
+"value"     		varchar(1000),
 units         varchar(100),
-type					varchar(100)
+"type"					varchar(100)
 );
 
 --HINT DISTRIBUTE_ON_RANDOM
 create table @synthea_schema.organizations (
 id			  varchar(1000),
 "name"	      varchar(1000),
-address       varchar(1000),
+"address"       varchar(1000),
 city		  varchar(100),
-state     	  varchar(100),
+"state"     	  varchar(100),
 zip           varchar(100),
 lat		numeric,
 lon 		numeric,
@@ -131,8 +131,8 @@ ssn           varchar(100),
 drivers       varchar(100),
 passport      varchar(100),
 prefix        varchar(100),
-first         varchar(100),
-last          varchar(100),
+"first"         varchar(100),
+"last"          varchar(100),
 suffix        varchar(100),
 maiden        varchar(100),
 marital       varchar(100),
@@ -140,9 +140,9 @@ race          varchar(100),
 ethnicity     varchar(100),
 gender        varchar(100),
 birthplace    varchar(100),
-address       varchar(100),
+"address"       varchar(100),
 city					varchar(100),
-state					varchar(100),
+"state"					varchar(100),
 county		varchar(100),
 zip						varchar(100),
 lat		numeric,
@@ -157,7 +157,7 @@ create table @synthea_schema.procedures (
 patient       varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(255),
+"description"   varchar(255),
 base_cost		numeric,
 reasoncode	varchar(1000),
 reasondescription	varchar(1000)
@@ -167,12 +167,12 @@ reasondescription	varchar(1000)
 create table @synthea_schema.providers (
 id varchar(1000),
 organization varchar(1000),
-name varchar(100),
+"name" varchar(100),
 gender varchar(100),
 speciality varchar(100),
-address varchar(255),
+"address" varchar(255),
 city varchar(100),
-state varchar(100),
+"state" varchar(100),
 zip varchar(100),
 lat numeric,
 lon numeric,
@@ -181,12 +181,12 @@ utilization numeric
 
 --HINT DISTRIBUTE_ON_RANDOM
 create table @synthea_schema.devices (
-start         date,
-stop          date,
+"start"         date,
+"stop"          date,
 patient       varchar(1000),
 encounter     varchar(1000),
 code          varchar(100),
-description   varchar(255),
+"description"   varchar(255),
 udi           varchar(255)
 );
 
